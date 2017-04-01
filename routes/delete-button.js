92eb11eab07032602b18d782a99f9f919831142b
Paul <dwkang@uga.edu>
2017-04-01 12:20:53 -0400

    a

--- routes/delete-button.js
+++
@@ -1,8 +0,0 @@
-<%  $(document).ready(()=>{ 
-								 		$('.delete-button').click({ 	
-								 			var shouldDelete = confirm('Are you sure you want to delete this task?')	
-								 			if(shouldDelete){	
-								 				window.location.href('/delete/ <%= task.id %>')	
-								 			}	
-								 		}) 
-								 	}) %>
